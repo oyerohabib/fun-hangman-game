a06f6469230d9ec44cf6036d82d803d9b1ec328a
oyerohabib <oyerohabib@yahoo.com>
2023-03-07 07:07:10 +0100

    feat: constructed  hangman figure with styles using individual images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Hangman from "./components/hangman";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <div className="title">Hangman Game</div>
-        <div className="figureWrapper"></div>
+        <div className="figureWrapper">
+          <Hangman incorrectPicks={6} />
+        </div>
         <div className="answerWrapper"></div>
       </div>
     );
